c7b22671d8ca827f0e9f2e08d64ba9f3ea592eb9
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-11-20 10:08:48 +0900

    다시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
-import { Button } from "@/stories/Button";
+'use client';
 
 export default function Home() {
-  return (
-    <>
-      <Button
-        label="스토리북 버"
-        primary
-        backgroundColor="red"
-        size="large"
-        onClick={() => {
-          alert("안녕?");
-        }}
-      />
-    </>
-  );
+  return <h1>메인 페이지</h1>;
 }
